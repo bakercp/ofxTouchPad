48000d18dac8507797342c44d15f25b6a40cd875
Christopher Baker <me@christopherbaker.net>
2015-07-28 22:21:56 -0500

    Cleanup.

---
+++ addon_config.mk
@@ -0,0 +1,8 @@
+meta:
+	ADDON_NAME = ofxTouchPad
+	ADDON_DESCRIPTION = OSX Multitouch driver.
+	ADDON_AUTHOR = @bakercp
+	ADDON_TAGS = "touchpad" "multitouch"
+	ADDON_URL = https://github.com/bakercp/ofxZeroconf
+common:
+		ADDON_DEPENDENCIES = ofxPointer
